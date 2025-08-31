b0647a5392e5e0754682692b86863ee9172ab52a
Melek78 <abemelek@Abemelek.localdomain>
2025-09-01 00:24:55 +0300

    admin/superuser

--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
 from .models import Book
+class BookAdmin(admin.ModelAdmin):  # 👈 This is required
+    list_display = ('title', 'author', 'publication_year')
+    list_filter = ('author', 'publication_year')
+    search_fields = ('title', 'author')
 
-admin.site.register(Book)
+admin.site.register(Book, BookAdmin)
