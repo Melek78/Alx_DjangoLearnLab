92e447f897b48ff72bb69eb92bda5e667894a659
Melek78 <abemelek78@gmail.com>
2025-09-08 17:52:40 +0300

    error fixing

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,21 +1,18 @@
 import os
 import django
 
-# Setup Django environment
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "LibraryProject.settings")
 django.setup()
 
 from relationship_app.models import Author, Book, Library, Librarian
 
-# Variables for queries
 author_name = "J.K. Rowling"
 library_name = "Central Library"
 
 def run_queries():
-    # 1. Query all books by a specific author
     try:
         author = Author.objects.get(name=author_name)
-        books_by_author = author.books.all()
+        books_by_author = Book.objects.filter(author=author)
         print(f"Books by {author.name}:")
         for book in books_by_author:
             print("-", book.title)
@@ -24,7 +21,6 @@ def run_queries():
 
     print("\n" + "-"*40 + "\n")
 
-    # 2. List all books in a library
     try:
         library = Library.objects.get(name=library_name)
         books_in_library = library.books.all()
@@ -36,7 +32,6 @@ def run_queries():
 
     print("\n" + "-"*40 + "\n")
 
-    # 3. Retrieve the librarian for a library
     try:
         library = Library.objects.get(name=library_name)
         librarian = library.librarian
