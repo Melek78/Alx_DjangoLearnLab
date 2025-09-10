9b6270b0c7718c0f55b0730cada31587c6d3b88d
Melek78 <abemelek78@gmail.com>
2025-09-10 13:30:05 +0300

    error fixing

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -4,7 +4,7 @@ from .models import Book
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
-
+from django.contrib.auth import login
 from django.views.generic.detail import DetailView
 from .models import Library
 
