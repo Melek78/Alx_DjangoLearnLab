eb6b22e3d3928444b03dcd8470e62b9ce71fa4a6
Melek78 <abemelek78@gmail.com>
2025-09-15 14:03:46 +0300

    error fixing

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import permission_required
+from .models import Book
 
-# Create your views here.
+# Function-based view for listing books
+@permission_required('bookshelf.can_view_book', raise_exception=True)
+def book_list(request):
+    books = Book.objects.all()  # the 'books' the checker expects
+    return render(request, 'bookshelf/book_list.html', {'books': books})
