0ea9131b6a475cef09a287ddc399b380b3ac6d4f
Melek78 <abemelek78@gmail.com>
2025-09-24 15:18:14 +0300

    error fixing

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from .views import BookListCreateAPIView
+from .views import BookListAPIView
 
 urlpatterns = [
-    path('books/', BookListCreateAPIView.as_view(), name='book-list'),
+    path('books/', BookListAPIView.as_view(), name='book-list'),
 ]
