31b7264901cc74d1edddcde91eb1d2d7d65e96d8
Melek78 <abemelek78@gmail.com>
2025-09-24 20:25:18 +0300

    authentication

--- api_project/api_project/settings.py
+++ api_project/api_project/settings.py
@@ -39,7 +39,20 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'api',
+    'rest_framework.authtoken',
 ]
+# Token authentication enabled
+# Permission: Only authenticated users can access Book endpoints
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
+}
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
