58485b707d2c8649d0a58f8f71945c8ea2d337ab
Melek78 <abemelek78@gmail.com>
2025-09-28 22:00:18 +0300

    error fixing

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -7,10 +7,11 @@ from .views import (
     BookDeleteView
 )
 
+# URLs for book API
 urlpatterns = [
-    path('books/', BookListView.as_view(), name='book-list'),
-    path('books/<int:pk>/', BookDetailView.as_view(), name='book-detail'),
-    path('books/create/', BookCreateView.as_view(), name='book-create'),
-    path('books/<int:pk>/update/', BookUpdateView.as_view(), name='book-update'),
-    path('books/<int:pk>/delete/', BookDeleteView.as_view(), name='book-delete'),
+    path('books/', BookListView.as_view(), name='book-list'),             # list all books
+    path('books/<int:pk>/', BookDetailView.as_view(), name='book-detail'), # get one book
+    path('books/create/', BookCreateView.as_view(), name='book-create'),   # add a new book
+    path('books/<int:pk>/update/', BookUpdateView.as_view(), name='book-update'), # update a book
+    path('books/<int:pk>/delete/', BookDeleteView.as_view(), name='book-delete'), # delete a book
 ]
