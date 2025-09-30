ae1fd5d226ad797f2bd8d04e422df1d163f3664d
Melek78 <abemelek78@gmail.com>
2025-09-30 13:33:43 +0300

    error fix

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -1,13 +1,19 @@
 from django.urls import path
-from .views import (
-    BookListCreateView,
-    BookRetrieveUpdateDeleteView
-)
+from .views import BookListView, BookDetailView, BookCreateView, BookUpdateView, BookDeleteView 
 
 urlpatterns = [
-    # List all books (GET) and Create a new book (POST)
-    path('books/', BookListCreateView.as_view(), name='book-list-create'),
+    # List all books
+    path('books/', BookListView.as_view(), name='book-list'),
 
-    # Retrieve (GET), Update (PUT/PATCH), Delete (DELETE) a single book by ID
-    path('books/<int:pk>/', BookRetrieveUpdateDeleteView.as_view(), name='book-detail-update-delete'),
+    # Retrieve details of a single book
+    path('books/<int:pk>/', BookDetailView.as_view(), name='book-detail'),
+
+    # Create a new book
+    path('books/create/', BookCreateView.as_view(), name='book-create'),
+
+    # Update an existing book
+    path('books/<int:pk>/update/', BookUpdateView.as_view(), name='book-update'),
+
+    # Delete a book
+    path('books/<int:pk>/delete/', BookDeleteView.as_view(), name='book-delete'),
 ]
--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -58,4 +58,3 @@ class BookDeleteView(generics.DestroyAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     permission_classes = [permissions.IsAuthenticated]
-
