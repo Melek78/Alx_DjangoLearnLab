62a4e9d833d29ca7b683b74b8eae521d117882d1
Melek78 <abemelek78@gmail.com>
2025-09-30 15:17:43 +0300

    last error for unittest

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
-from rest_framework import generics, filters, dj
+from rest_framework import generics, filters
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 from .models import Book
 from .serializers import BookSerializer
-from django_filters import rest_framework
+# from django_filters import rest_framework
 from django_filters.rest_framework import DjangoFilterBackend
 
 class BookListView(generics.ListAPIView):
@@ -16,7 +16,7 @@ class BookListView(generics.ListAPIView):
     serializer_class = BookSerializer
     permission_classes = [IsAuthenticatedOrReadOnly]  # Read-only for all users
     filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
-    
+
     filterset_fields = ['title', 'author', 'publication_year']
     search_fields = ['title', 'author__name']
     ordering_fields = ['title', 'publication_year', 'author__name', 'genre']
