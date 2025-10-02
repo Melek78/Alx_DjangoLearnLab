942a61b8e32014b2b8b67fe526152835268705dd
Melek78 <abemelek78@gmail.com>
2025-10-02 21:44:53 +0300

    views doesnt contain post error fix

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.urls import reverse_lazy, reverse
 from django.contrib.auth.views import LoginView, LogoutView
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView
 from .forms import RegisterForm, UserUpdateForm, ProfileUpdateForm
 from django.contrib.auth import login, logout, authenticate
 from django.contrib.auth.mixins import LoginRequiredMixin
@@ -17,6 +17,8 @@ class RegisterView(CreateView):
         user = form.save()
         login(self.request, user)
         return super().form_valid(form)
+    def post(self, request, *args, **kwargs):
+        return super().post(request, *args, **kwargs)
 
 class CustomLoginView(LoginView):
     template_name = 'blog/login.html'
@@ -26,4 +28,13 @@ class CustomLogoutView(LogoutView):
 
 class ProfileView(LoginRequiredMixin, TemplateView):
     template_name = 'blog/profile.html'
+    form_class = ProfileUpdateForm
+    success_url = reverse_lazy('profile')
     login_url = 'login'
+
+    def get_object(self, queryset=None):
+        return self.request.user
+
+    def post(self, request, *args, **kwargs):
+        messages.success(request, "Profile updated successfully")
+        return super().post(request, *args, **kwargs)
