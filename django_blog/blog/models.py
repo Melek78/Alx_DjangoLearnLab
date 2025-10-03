9ea29b3bce8c7db4b25af4eeac6e00cdd1b3991a
Melek78 <abemelek78@gmail.com>
2025-10-03 17:04:54 +0300

    created at error in models

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -34,7 +34,7 @@ class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='comments')
     content = models.TextField()
-    published_date = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     class Meta:
